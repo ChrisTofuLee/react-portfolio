9b55e687a295ba336e5414bf08fec3c1a021c8ae
Selfay <chris.lee90@hotmail.co.uk>
2020-09-09 18:36:43 +0100

    Updates

---
+++ precache-manifest.b28087d6bba2e2f62199f32ed6f30d31.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b315603f24de5bd5ff6fc4b1a047dde",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "dab76a2e03f60a13c2d7",
+    "url": "/react-portfolio/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "ae3ddce6cf31d3567f9c",
+    "url": "/react-portfolio/static/css/main.1d5633e7.chunk.css"
+  },
+  {
+    "revision": "dab76a2e03f60a13c2d7",
+    "url": "/react-portfolio/static/js/2.44bbe894.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.44bbe894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3ddce6cf31d3567f9c",
+    "url": "/react-portfolio/static/js/main.1002713a.chunk.js"
+  },
+  {
+    "revision": "c37ef703e25eadd8948f",
+    "url": "/react-portfolio/static/js/runtime-main.b61b73f6.js"
+  },
+  {
+    "revision": "f99563c6ba7781943a26ee052be5898f",
+    "url": "/react-portfolio/static/media/Notes.f99563c6.png"
+  },
+  {
+    "revision": "6d29ab5d32f7e298b208bb5b72b1800f",
+    "url": "/react-portfolio/static/media/aniTracker.6d29ab5d.png"
+  },
+  {
+    "revision": "bfb8aa8fbce5df839ca2a0430bb74d0d",
+    "url": "/react-portfolio/static/media/cli.bfb8aa8f.png"
+  },
+  {
+    "revision": "cd65809680f74981bcf56f4f0aed2254",
+    "url": "/react-portfolio/static/media/employee.cd658096.png"
+  },
+  {
+    "revision": "f1afb58e69345b5a0958dd279bc2f594",
+    "url": "/react-portfolio/static/media/lyric.f1afb58e.png"
+  },
+  {
+    "revision": "815e37165be25d914776dcd76aeb421e",
+    "url": "/react-portfolio/static/media/quiz.815e3716.png"
+  },
+  {
+    "revision": "0c57bcb634c1b4e3567b70f41a5e9af6",
+    "url": "/react-portfolio/static/media/stars.0c57bcb6.jpg"
+  }
+]);
